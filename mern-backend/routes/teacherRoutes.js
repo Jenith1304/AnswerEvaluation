677f7d5f9b145e19823d06684ff978231efc208b
Krish Patel <kp4221111@gmail.com>
2025-06-19 16:14:01 +0530

    resolving merge devKrish

--- mern-backend/routes/teacherRoutes.js
+++ mern-backend/routes/teacherRoutes.js
@@ -1,21 +1,17 @@
 const express = require('express')
 const teacherMiddleware = require('../middlewares/teacherMiddleware')
-const { createTest, deleteTest, getAllTests, getAllQuestions, updateQuestionInTest, addQuestionToTest, removeQuestionFromTest, extractImagesFromPDF } = require('../controllers/TeacherController')
-
+const { createTest, deleteTest, getAllTests, getAllQuestions, updateQuestionInTest, addQuestionToTest, removeQuestionFromTest, extractImagesFromPDF ,uploadAnswerSheet} = require('../controllers/TeacherController')
 const teacherRouter = express.Router()
-
 const upload = require('../middlewares/uploadAnswersheet')
 
 teacherRouter.post('/createTest', teacherMiddleware, createTest)
 teacherRouter.post('/deletetest/:id', teacherMiddleware, deleteTest)
-teacherRouter.post('/createTest', teacherMiddleware, createTest)
 teacherRouter.get('/getAllTests', teacherMiddleware, getAllTests)
 teacherRouter.get("/test/:testId/questions", teacherMiddleware, getAllQuestions);
 teacherRouter.put("/test/:testId/question/:questionId", teacherMiddleware, updateQuestionInTest);
 teacherRouter.post("/test/:testId/", teacherMiddleware, addQuestionToTest);
 teacherRouter.post("/test/:testId/question/:questionId", teacherMiddleware, removeQuestionFromTest);
 teacherRouter.get("/extractImagesFromPDF", teacherMiddleware, extractImagesFromPDF);
-teacherRouter.post('/deletetest', teacherMiddleware, deleteTest)
 teacherRouter.post('/upload/:studentId/:testId', teacherMiddleware, upload.single('file'), uploadAnswerSheet);
 
 
